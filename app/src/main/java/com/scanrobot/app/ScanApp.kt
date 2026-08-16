ff293079a59f158455ea1941faf19e413002c584
qq145968 <92039946+qq145968@users.noreply.github.com>
2026-08-16 11:32:04 +0800

    fix: add crash handler, network security config, bump version to 1.4.0

--- app/src/main/java/com/scanrobot/app/ScanApp.kt
+++ app/src/main/java/com/scanrobot/app/ScanApp.kt
@@ -1,6 +1,7 @@
 package com.scanrobot.app
 
 import android.app.Application
+import android.util.Log
 import com.scanrobot.app.data.ScanStore
 
 class ScanApp : Application() {
@@ -11,6 +12,19 @@ class ScanApp : Application() {
         super.onCreate()
         instance = this
         scanStore = ScanStore(this)
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            Log.e("ScanRobot_CRASH", "Uncaught exception on ${thread.name}", throwable)
+            val sw = java.io.StringWriter()
+            throwable.printStackTrace(java.io.PrintWriter(sw))
+            val crashLog = sw.toString()
+            getSharedPreferences("crash_log", MODE_PRIVATE)
+                .edit()
+                .putString("last_crash", crashLog)
+                .putLong("crash_time", System.currentTimeMillis())
+                .apply()
+            android.os.Process.killProcess(android.os.Process.myPid())
+        }
     }
 
     companion object {
