34803123fd63ac7c0747d35bf44b03fa91ba603b
qq145968 <92039946+qq145968@users.noreply.github.com>
2026-08-16 11:50:42 +0800

    fix: crash log to file + dialog, simplify BackHandler, add debug logs

--- app/src/main/java/com/scanrobot/app/ScanApp.kt
+++ app/src/main/java/com/scanrobot/app/ScanApp.kt
@@ -3,6 +3,7 @@ package com.scanrobot.app
 import android.app.Application
 import android.util.Log
 import com.scanrobot.app.data.ScanStore
+import java.io.File
 
 class ScanApp : Application() {
     lateinit var scanStore: ScanStore
@@ -11,20 +12,35 @@ class ScanApp : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-        scanStore = ScanStore(this)
+
+        val previousHandler = Thread.getDefaultUncaughtExceptionHandler()
 
         Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
             Log.e("ScanRobot_CRASH", "Uncaught exception on ${thread.name}", throwable)
+
             val sw = java.io.StringWriter()
             throwable.printStackTrace(java.io.PrintWriter(sw))
-            val crashLog = sw.toString()
+            val crashLog = "Time: ${java.util.Date()}\nThread: ${thread.name}\n\n$sw"
+
+            // Write to SharedPreferences synchronously
             getSharedPreferences("crash_log", MODE_PRIVATE)
                 .edit()
                 .putString("last_crash", crashLog)
                 .putLong("crash_time", System.currentTimeMillis())
-                .apply()
-            android.os.Process.killProcess(android.os.Process.myPid())
+                .commit()
+
+            // Also write to a file in app's external files dir (visible in file manager)
+            try {
+                val logFile = File(getExternalFilesDir(null), "crash_log.txt")
+                logFile.writeText(crashLog)
+            } catch (e: Exception) {
+                Log.e("ScanRobot_CRASH", "Failed to write crash file", e)
+            }
+
+            previousHandler?.uncaughtException(thread, throwable)
         }
+
+        scanStore = ScanStore(this)
     }
 
     companion object {
